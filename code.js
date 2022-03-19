60f00cdef6823b424d6535161a95a06b4ebaa22e
John Irle <irlejohn@gmail.com>
2022-03-19 16:31:22 -0500

    feat: control line height value with range and only apply when checked.

--- code.js
+++ code.js
@@ -1,5 +1,37 @@
 let centerButton = document.getElementById("centerButton");
 let lineHeightButton = document.getElementById("lineHeightButton");
+let lineHeight = document.getElementById("lineHeight")
+let lineHeightCheck = document.querySelector("input[name=applyLineHeight]");
+
+lineHeightCheck.addEventListener("change", async () => {
+    let label = document.getElementById("lineHeightLabel");
+    label.innerHTML = `Line Height: ${lineHeight.value}`;
+
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if(lineHeightCheck.checked) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: applyLineHeight,
+            args: [lineHeight.value]
+        });
+    }
+})
+
+lineHeight.addEventListener("input", async () => {
+    let label = document.getElementById("lineHeightLabel");
+    label.innerHTML = `Line Height: ${lineHeight.value}`;
+
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if(lineHeightCheck.checked) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: applyLineHeight,
+            args: [lineHeight.value]
+        });
+    }
+})
 
 centerButton.addEventListener("click", async() => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -10,20 +42,11 @@ centerButton.addEventListener("click", async() => {
     });
 });
 
-lineHeightButton.addEventListener("click", async() => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: applyLineHeight,
-    });
-});
-
 function applyCentering() {
     document.body.style.maxWidth = "75%";
     document.body.style.margin = "0 auto";
 }
 
-function applyLineHeight() {
-    document.body.style.lineHeight = "1.5";
+function applyLineHeight(value) {
+    document.body.style.lineHeight = `${value}`;
 }
