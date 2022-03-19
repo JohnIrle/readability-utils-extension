30f1582b912ecc65b5380db84af37ec6bb6ce83a
John Irle <irlejohn@gmail.com>
2022-03-19 18:23:25 -0500

    feat: use range for width and line height.  (#3)
    * feat: control line height value with range and only apply when checked.
    
    * feat: group styles.
    
    * feat: use width slider and format.
    
    * feat: change width with slider.
    
    * refactor: change maxWidth to width.
    
    * feat: group check and label and increase line height range.
    
    * feat: align check and label.

--- code.js
+++ code.js
@@ -1,29 +1,52 @@
-let centerButton = document.getElementById("centerButton");
-let lineHeightButton = document.getElementById("lineHeightButton");
+let width = document.getElementById("width");
+let lineHeight = document.getElementById("lineHeight");
+let lineHeightCheck = document.querySelector("input[name=applyLineHeight]");
 
-centerButton.addEventListener("click", async() => {
+width.addEventListener("input", async () => {
+    let label = document.getElementById("widthLabel");
+    label.innerHTML = `width: ${width.value}%`;
+
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: applyCentering,
+        function: applyWidth,
+        args: [width.value],
     });
 });
 
-lineHeightButton.addEventListener("click", async() => {
+lineHeightCheck.addEventListener("change", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: applyLineHeight,
-    });
+    if (lineHeightCheck.checked) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: applyLineHeight,
+            args: [lineHeight.value],
+        });
+    }
 });
 
-function applyCentering() {
-    document.body.style.maxWidth = "75%";
+lineHeight.addEventListener("input", async () => {
+    let label = document.getElementById("lineHeightLabel");
+    label.innerHTML = `Line Height: ${lineHeight.value}`;
+
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (lineHeightCheck.checked) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: applyLineHeight,
+            args: [lineHeight.value],
+        });
+    }
+});
+
+function applyWidth(value) {
+    document.body.style.width = `${value}%`;
     document.body.style.margin = "0 auto";
 }
 
-function applyLineHeight() {
-    document.body.style.lineHeight = "1.5";
+function applyLineHeight(value) {
+    document.body.style.lineHeight = `${value}`;
 }
