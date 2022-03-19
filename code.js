1e3527ebe512ba8c3e99d9a720f57fb6d17fb75a
John Irle <irlejohn@gmail.com>
2022-03-19 17:40:07 -0500

    feat: change width with slider.

--- code.js
+++ code.js
@@ -1,22 +1,31 @@
-let centerButton = document.getElementById("centerButton");
-let lineHeightButton = document.getElementById("lineHeightButton");
-let lineHeight = document.getElementById("lineHeight")
+let width = document.getElementById("width");
+let lineHeight = document.getElementById("lineHeight");
 let lineHeightCheck = document.querySelector("input[name=applyLineHeight]");
 
-lineHeightCheck.addEventListener("change", async () => {
-    let label = document.getElementById("lineHeightLabel");
-    label.innerHTML = `Line Height: ${lineHeight.value}`;
+width.addEventListener("input", async () => {
+    let label = document.getElementById("widthLabel");
+    label.innerHTML = `width: ${width.value}%`;
 
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    if(lineHeightCheck.checked) {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: applyWidth,
+        args: [width.value],
+    });
+});
+
+lineHeightCheck.addEventListener("change", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (lineHeightCheck.checked) {
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: applyLineHeight,
-            args: [lineHeight.value]
+            args: [lineHeight.value],
         });
     }
-})
+});
 
 lineHeight.addEventListener("input", async () => {
     let label = document.getElementById("lineHeightLabel");
@@ -24,26 +33,17 @@ lineHeight.addEventListener("input", async () => {
 
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    if(lineHeightCheck.checked) {
+    if (lineHeightCheck.checked) {
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: applyLineHeight,
-            args: [lineHeight.value]
+            args: [lineHeight.value],
         });
     }
-})
-
-centerButton.addEventListener("click", async() => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: applyCentering,
-    });
 });
 
-function applyCentering() {
-    document.body.style.maxWidth = "75%";
+function applyWidth(value) {
+    document.body.style.maxWidth = `${value}%`;
     document.body.style.margin = "0 auto";
 }
 
